78bbfe2b0812c66d1a3335065d66fb3154b10f22
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-03 17:44:32 -0700

    Developed interface to add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
+import {ITask} from './Interfaces'
 
 const App: FC  = () => {
   const [task, setTask] = useState<string>("")
   const [deadline, setDeadline] = useState<number>(0)
-  const [todo, setTodo] = useState([])
+  const [todoList, setTodoList] = useState<ITask[]>([])
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
       setTask(event.target.value)
     } else {
       setDeadline(Number(event.target.value))
     }
   }
+
+  const addTask = (): void => {
+    const newTask = {taskName: task, deadline: deadline};
+    setTodoList([...todoList, newTask ])
+    console.log(todoList)
+  }
+
 
   return (
     <div className="App">
@@ -21,7 +31,7 @@ const App: FC  = () => {
         <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
         <input type="number" placeholder="Deadline in days..." name="task" onChange={handleChange}  />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="todolist">
 
