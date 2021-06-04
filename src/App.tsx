ebf67f1af3d1dc117b354bef6394483b1ea65761
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-03 17:31:24 -0700

    Defined type defs for useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
 
 const App: FC  = () => {
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodo] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-        <input type="text" placeholder="Task..." />
-        <input type="number" placeholder="Deadline in days..." />
+        <input type="text" placeholder="Task..." onChange={handleChange} />
+        <input type="number" placeholder="Deadline in days..." onChange={handleChange}  />
         </div>
         <button>Add Task</button>
       </div>
