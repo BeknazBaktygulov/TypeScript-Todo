5f8d6d37a1254cfd1d7fb90258e105865c789ad1
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-06-03 17:17:12 -0700

    Added some generic elements

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ import './App.css';
 const App: FC  = () => {
   return (
     <div className="App">
-      <h1>Hello TypeScripters</h1>
+      <div className="header">
+        <div className="inputContainer">
+        <input type="text" placeholder="Task..." />
+        <input type="number" placeholder="Deadline in days..." />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todolist">
+
+        </div>
     </div>
   );
 }
